c3fe4a616d7a55f06ac1025fdd606688a38cf043
kirill <chiril.ghermec@gmail.com>
2021-11-19 21:57:30 +0200

    Rebuild : v2.0.1

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/*.html'],
+  corePlugins: {
+    preflight: false
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px'
+      }
+    },
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
